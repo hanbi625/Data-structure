b33b7d42a42d1ecc9ef83648f20ad90daebcfe03
이맘비 <80617446+hanbi625@users.noreply.github.com>
2023-02-23 15:55:43 +0900

    Create 3.c

---
+++ 3주차 실습(재귀)/3.c
@@ -0,0 +1,20 @@
+#pragma warning (disable : 4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void rdigit(int);
+int main() {
+	int N;
+
+	scanf("%d", &N);
+	rdigit(N);
+
+	return 0;
+}
+void rdigit(int N) {
+	if (N < 10) printf("%d\n", N);
+	else {
+		printf("%d\n", N % 10);
+		rdigit(N / 10);
+	}
+}
